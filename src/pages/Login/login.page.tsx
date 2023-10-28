787d56bde86c07ba576eab0e3cb24375eb33f548
chikyuuWIP <wittaya.prombunjong@gmail.com>
2023-10-28 23:48:17 +0700

    [003] add form handle on login page and register page

--- src/pages/Login/login.page.tsx
+++ src/pages/Login/login.page.tsx
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 import "./login.page.css";
 import { NavLink } from "react-router-dom";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = React.useState("password");
-  return (  
+  const loginFormRef = useRef<HTMLFormElement | null>(null);
+
+  const loginHandle = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (loginFormRef.current) {
+      const usernameInput = loginFormRef.current.username as
+        | HTMLInputElement
+        | undefined;
+      const passwordInput = loginFormRef.current.password as
+        | HTMLInputElement
+        | undefined;
+
+      if (usernameInput && passwordInput) {
+        console.log("Username:", usernameInput.value);
+        console.log("Password:", passwordInput.value);
+      }
+    }
+  };
+
+  return (
     <div className="page">
-      <div className="card">
+      <form ref={loginFormRef} onSubmit={loginHandle} className="card">
         <h1>Sign in</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+          required
+          autoComplete="on"
+        />
+        <input
+          type={showPassword}
+          id="password"
+          name="password"
+          placeholder="Password"
+          required
+          autoComplete="on"
+        />
+        {showPassword == "password" ? (
+          <VisibilityIcon
+            className="visible-icon"
+            onClick={() => {
+              setShowPassword("text");
+            }}
+          />
+        ) : (
+          <VisibilityOffIcon
+            className="visible-icon"
+            onClick={() => {
+              setShowPassword("password");
+            }}
+          />
+        )}
+
         <button>Sign in</button>
         <p>
           Don't have an account? Register <NavLink to="/register">here</NavLink>
         </p>
-      </div>
+      </form>
     </div>
   );
 };
--- src/pages/Register/register.page.tsx
+++ src/pages/Register/register.page.tsx
@@ -1,23 +1,96 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import "./register.page.css";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export const RegisterPage = () => {
+  const [showPassword, setShowPassword] = React.useState("password");
+  const [alertMessage, setAlertMessage] = React.useState("");
+  const date = new Date();
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", {
+    month: "2-digit",
+  });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+  const todayDate = [year, month, day].join("-");
+  const registerFormRef = useRef<HTMLFormElement | null>(null);
+
+  const registerHandle = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (registerFormRef.current) {
+      const usernameInput = registerFormRef.current.username as
+        | HTMLInputElement
+        | undefined;
+      const emailInput = registerFormRef.current.email as
+        | HTMLInputElement
+        | undefined;
+      const passwordInput = registerFormRef.current.password as
+        | HTMLInputElement
+        | undefined;
+      const confirmPasswordInput = registerFormRef.current
+        .confirmPasswordInput as HTMLInputElement | undefined;
+      const dateOfBirth = registerFormRef.current.dateOfBirth as
+        | HTMLInputElement
+        | undefined;
+    }
+  };
+
   return (
     <div className="page-register">
-      <div className="card">
+      <form className="card" ref={registerFormRef} onSubmit={registerHandle}>
         <h1>Sign up</h1>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <input type="text" placeholder="Confirm Password" />
-        <p className="bd-label">Birth Date</p>
-        <input type="date" className="birthdate" placeholder="dd-mm-yyyy" />
-
+        <input type="text" id="username" placeholder="Username" required />
+        <input type="email" id="email" placeholder="Email" required />
+        <input
+          type={showPassword}
+          id="password"
+          placeholder="Password"
+          autoComplete="off"
+          required
+        />
+        {showPassword == "password" ? (
+          <VisibilityIcon
+            className="visible-icon"
+            onClick={() => {
+              setShowPassword("text");
+            }}
+          />
+        ) : (
+          <VisibilityOffIcon
+            className="visible-icon"
+            onClick={() => {
+              setShowPassword("password");
+            }}
+          />
+        )}
+        <p></p>
+        <input
+          type={showPassword}
+          id="confirmPassword"
+          placeholder="Confirm Password"
+          autoComplete="off"
+          required
+        />
+        <span style={{ visibility: alertMessage == "" ? "hidden" : "visible" }}>
+          {alertMessage}
+        </span>
+        <div className="bd-div">
+          <label htmlFor="birthdate" className="bd-label">
+            Birth Date
+          </label>
+          <input
+            type="date"
+            className="bd-datepicker"
+            max={todayDate}
+            required
+          />
+        </div>
         <button>Sign up</button>
         <p>
           already have an account? Login <NavLink to="/login">here</NavLink>
         </p>
-      </div>
+      </form>
     </div>
   );
 };
